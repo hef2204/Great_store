215b9fb5a2c57598d7eec14a3c3ed0cad4726bd5
Boris heifits <hef1995@gmail.com>
2024-01-30 18:03:12 +0200

    change2

--- views/product.py
+++ views/product.py
@@ -1,32 +1,32 @@
 from flask import  request, Blueprint
 
-product_blueprint = Blueprint("product", __name__)
+bp = Blueprint("product", __name__, url_prefix="/products")
 
 
 
-@product_blueprint.route("/product")
+@bp.route("")
 def product_list():
     return "List of all products"
 
 
-@product_blueprint.route("/product/<product_id>")
+@bp.route("/<product_id>")
 def product_page(product_id):
     product_id = request.args.get("id") 
     return f"page for product with id {product_id}"
 
 
-@product_blueprint.route("/product/create", methods=["GET", "POST"])
+@bp.route("/create", methods=["GET", "POST"])
 def product_create():
     if request.method == "GET":
         return "product create form"
     elif request.method == "POST":
         return "create product successfully" 
 
-@product_blueprint.route("/product/<product_id>", methods=["DELETE"])
+@bp.route("/<product_id>", methods=["DELETE"])
 def product_delete(product_id):
     return f"deleted product successfully {product_id}"
 
-@product_blueprint.route("/product/<product_id>", methods=["PATCH"])
+@bp.route("/<product_id>", methods=["PATCH"])
 def product_update(product_id):
     updated_product = request.json("id")
     return f"updated product {product_id} with data {updated_product}"
--- views/user.py
+++ views/user.py
@@ -1,28 +1,28 @@
 from flask import  request, Blueprint
 
-user_blueprint = Blueprint("user", __name__)
+bp = Blueprint("user", __name__)
 
 
-@user_blueprint.route("/user")
+@bp.route("/user")
 def user_list():
     return "user list"
 
-@user_blueprint.route("/user/<user_id>")
+@bp.route("/user/<user_id>")
 def user_page(user_id):
     return f"page for user {user_id}"
 
-@user_blueprint.route("/user/create", methods=["GET", "POST"])
+@bp.route("/user/create", methods=["GET", "POST"])
 def user_create():
     if request.method == "GET":
         return "user create form"
     elif request.method == "POST":
         return "created user successfully"
     
-@user_blueprint.route("/user/<user_id>", methods=["DELETE"])
+@bp.route("/user/<int:user_id>", methods=["DELETE"])
 def user_delete(user_id):
     return f"deleted user successfully {user_id}"
 
-@user_blueprint.route("/user/<user_id>", methods=["PATCH"])
+@bp.route("/user/<int:user_id>", methods=["PATCH"])
 def user_update(user_id):
     updated_user = request.json("id")
     return f"updated user {user_id} with data {updated_user}"
